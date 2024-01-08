4ecb09c92ec4e677e2d4d34ea10f25b96a76c15a
Himangivaghasiya12 <gopivaghasiya001@gmail.com>
2024-01-08 21:43:28 +0530

    first commit

---
+++ in_prefix.c
@@ -0,0 +1,21 @@
+r. no.  	stack	        Expression Prefix ( Reversed )
+0		    (	
+1	(	    ( (	
+2	F	    ( (                 F
+3	-	    ( ( -	            F
+4	(	    ( ( - (	            F
+5	E	    ( ( - (	            FE
+6	/	    ( ( - ( /	        FE
+7	D	    ( ( - ( /	        FED
+8	)	    ( ( -	            FED/
+9	*	    ( ( - *	            FED/
+10	(	    ( ( - * (	        FED/
+11	C	    ( ( - * (	        FED/C
+12	-	    ( ( - * ( -	        FED/C
+13	B	    ( ( - * ( -	        FED/CB
+14	)	    ( ( - *	            FED/CB-
+15	+	    ( ( - +	            FED/CB-*
+16		    ( ( - +	            FED/CB-*
+17	A	    ( ( - +	            FED/CB-* A
+18	)	    (	                FED/CB-* A+-
+19	)		                    FED/CB-* A+-
